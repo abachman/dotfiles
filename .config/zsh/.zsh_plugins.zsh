8f3dbd61d4e7db83121cf76ee06eb38f03b6f0e8
Adam Bachman <adam.bachman@gmail.com>
2024-04-14 11:46:43 -0400

    setting up antidote and powerlevel10k

---
+++ .config/zsh/.zsh_plugins.zsh
@@ -0,0 +1,1 @@
+fpath+=( /Users/adambachman/Library/Caches/antidote/https-COLON--SLASH--SLASH-github.com-SLASH-romkatv-SLASH-powerlevel10k )
---
+++ .config/zsh/input.zsh
@@ -0,0 +1,3 @@
+bindkey -e
+bindkey '^[[1;9C' forward-word
+bindkey '^[[1;9D' backward-word
