96f1d256a5b00210f2bc9bcd432499f999f42762
Adam Bachman <abachman@recurly.com>
2024-05-20 11:10:33 -0400

    unignore local.zsh since we're using machine-specific branches

---
+++ .config/zsh/local.zsh
@@ -0,0 +1,21 @@
+# work settings on the work machine, personal settings on the personal machine
+export PATH="$HOME/workspace/dotfiles/bin:$PATH"
+export PATH="$HOME/workspace/developer-setup/bin:$PATH"
+export PATH="$HOME/.local/bin:$PATH" # poetry
+export PATH="/opt/homebrew/opt/openssl@3/bin:$PATH"
+export PATH="$HOME/go/bin:$PATH"
+
+# don't need to navigate to ~/ to cd into Downloads, etc.
+export CDPATH=".:$HOME:$HOME/workspace:$HOME/src"
+
+# add local completions
+fpath=(~/.config/zshrc/completions $fpath)
+
+source $ZDOTDIR/recurly.zsh
+
+# Google Cloud SDK.
+export GCLOUD_SDK=$HOME/src/google-cloud-sdk
+export USE_GKE_GCLOUD_AUTH_PLUGIN=True
+export CLOUDSDK_PYTHON=$PYENV_ROOT/shims/python
+if [ -f "$GCLOUD_SDK/path.zsh.inc" ]; then . "$GCLOUD_SDK/path.zsh.inc"; fi
+if [ -f "$GCLOUD_SDK/completion.zsh.inc" ]; then . "$GCLOUD_SDK/completion.zsh.inc"; fi
